3d8cef238e4458e84e06aa4a6caf33a0591086c4
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-11-09 19:07:16 +0100

    WIP: add function for l-system

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,76 @@ import { Camera, Clock, Mesh, MeshNormalMaterial, Renderer, Scene } from 'three'
 
 let camera: Camera
 let scene: Scene
-let geometry: THREE.BoxGeometry
-let material: MeshNormalMaterial
 let renderer: Renderer
-let mesh: Mesh
+let line: THREE.Line
 
 let clock: Clock = new THREE.Clock()
 
+let current: String = "A"
+let count: number = 0
+
+function makeTree() {
+    let next: String[] = [];
+
+    for (let index = 0; index < current.length; index++) {
+        let c: String = current[index]
+        
+        if(c === 'A')
+            next.push("ABA")
+        else if(c === 'B')
+            next.push("BBB")
+    }
+
+    current = next.join("");
+    count++;
+    console.log("Generation " + count + ": " + current)
+}
+
+console.log("Generation " + count + ": " + current)
+window.addEventListener("click", makeTree);
+
 init()
-animate()
+//animate()
 
 function init() {
-    camera = new THREE.PerspectiveCamera(
-        70,
-        window.innerWidth / window.innerHeight,
-        0.01,
-        10
-    )
-    camera.position.z = 1
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+    camera.position.set( 0, 0, 100 );
+    camera.lookAt( 0, 0, 0 );
 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
-    material = new THREE.MeshNormalMaterial()
+    scene = new THREE.Scene();
 
-    mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
+    //create a blue LineBasicMaterial
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+    line = new THREE.Line( geometry, material );
+
+    scene.add( line );
+    renderer.render( scene, camera );
 }
 
-function animate() {
-    requestAnimationFrame(animate)
-    let delta: number = clock.getDelta()
+// function animate() {
+//     requestAnimationFrame(animate)
+//     let delta: number = clock.getDelta()
 
-    mesh.rotation.y += 1 * delta
-    mesh.rotation.x += 0.8 * delta
+//     camera.lookAt(line.position)
+//     camera.rotation.y += 1* delta
 
-    mesh.position.y = 0.35 * Math.sin(1.3 * clock.getElapsedTime())
-    mesh.position.x = 0.5 * Math.cos(1.3 * clock.getElapsedTime())
+//     renderer.render(scene, camera)
 
-    renderer.render(scene, camera)
-}
+//     // mesh.rotation.y += 1 * delta
+//     // mesh.rotation.x += 0.8 * delta
+
+//     // mesh.position.y = 0.35 * Math.sin(1.3 * clock.getElapsedTime())
+//     // mesh.position.x = 0.5 * Math.cos(1.3 * clock.getElapsedTime())
+// }
