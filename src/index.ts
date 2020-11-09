2ddb9f0ba95111722056e55db88b01b173043e17
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-11-09 20:11:27 +0100

    WIP: add stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
-import { Camera, Clock, Mesh, MeshNormalMaterial, Renderer, Scene } from 'three'
+import { Camera, Clock, Mesh, MeshNormalMaterial, Renderer, Scene} from 'three'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 let camera: Camera
 let scene: Scene
 let renderer: Renderer
 let line: THREE.Line
+let controls: OrbitControls
 
 let clock: Clock = new THREE.Clock()
 
@@ -28,20 +30,29 @@ function makeTree() {
     console.log("Generation " + count + ": " + current)
 }
 
+function randomNumber(min: number, max: number) {  
+    return Math.random() * (max - min) + min; 
+}
+
 console.log("Generation " + count + ": " + current)
-window.addEventListener("click", makeTree);
+
+for (let index = 0; index < 3; index++) {
+    makeTree();
+}
 
 init()
-//animate()
+animate()
 
 function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-    camera.position.set( 0, 0, 100 );
-    camera.lookAt( 0, 0, 0 );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+
+    controls = new OrbitControls( camera, renderer.domElement );
+    camera.position.set( 0, 10, 10 );
+    controls.update();
 
     scene = new THREE.Scene();
 
@@ -49,9 +60,21 @@ function init() {
     const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
     const points = [];
-    points.push( new THREE.Vector3( - 10, 0, 0 ) );
-    points.push( new THREE.Vector3( 0, 10, 0 ) );
-    points.push( new THREE.Vector3( 10, 0, 0 ) );
+    let aTemp = 0;
+    let bTemp = 0;
+
+    for (let index = 0; index < current.length; index++) {
+        if(current[index] === "A") {
+            points.push( new THREE.Vector3( 0, aTemp, 0 ) );
+            aTemp = randomNumber(1, 4)
+            points.push( new THREE.Vector3( aTemp, 0, randomNumber(1, 7) ) );
+        }
+        else if(current[index] === "B") {
+            bTemp = randomNumber(1, 4)
+            points.push( new THREE.Vector3( 0, bTemp, 0 ) );
+            points.push( new THREE.Vector3( randomNumber(1, 5), 0, bTemp ) );
+        }
+    }
 
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
@@ -61,18 +84,17 @@ function init() {
     renderer.render( scene, camera );
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
-//     let delta: number = clock.getDelta()
+function animate() {
+    requestAnimationFrame(animate)
+    let delta: number = clock.getDelta()
 
-//     camera.lookAt(line.position)
-//     camera.rotation.y += 1* delta
+    controls.update();
 
-//     renderer.render(scene, camera)
+    renderer.render(scene, camera)
 
-//     // mesh.rotation.y += 1 * delta
-//     // mesh.rotation.x += 0.8 * delta
+    // mesh.rotation.y += 1 * delta
+    // mesh.rotation.x += 0.8 * delta
 
-//     // mesh.position.y = 0.35 * Math.sin(1.3 * clock.getElapsedTime())
-//     // mesh.position.x = 0.5 * Math.cos(1.3 * clock.getElapsedTime())
-// }
+    // mesh.position.y = 0.35 * Math.sin(1.3 * clock.getElapsedTime())
+    // mesh.position.x = 0.5 * Math.cos(1.3 * clock.getElapsedTime())
+}
