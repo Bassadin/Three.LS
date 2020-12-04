eda27187df1c52686bcdfce0d6df8f5e8dff19de
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-12-04 18:20:13 +0100

    TASK: add interface

--- src/index.ts
+++ src/index.ts
@@ -17,38 +17,81 @@ let clock: Clock = new THREE.Clock()
 let generations: number = 3
 let ruleset: Rule[] = []
 
-// Get Btns
-const btnAdd = document.querySelector('#btnAddRule')
+// Count all Rules
+let countAllRules = 1;
 
-// Get neccessary Elements
-const ruleWrapper = document.querySelector('#ruleWrapper')
+// Get Btns
+const btnAdd: HTMLInputElement = document.querySelector('#btnAddRule')
+const btnRemove: HTMLInputElement = document.querySelector('#btnRemoveRule')
+const btnUpload: HTMLInputElement = document.querySelector('#btnUpload')
+const btnDownload: HTMLInputElement = document.querySelector('#btnDownload')
+const btnGenerate: HTMLInputElement = document.querySelector('#btnGenerate')
+btnRemove.disabled = true;
+
+//Necessary Elements
+const rulesWrapper: HTMLDivElement = document.querySelector('#rulesWrapper')
 
 // Eventlisteners for Btns
-btnAdd.addEventListener('click', addNewRuleField)
+btnAdd.addEventListener('click', () => {
+    rulesWrapper.insertAdjacentHTML('beforeend', ' <div class="interface__rule-wrapper" style="margin-top: 1rem;" id="count' + (countAllRules + 1) + '"> <div class="interface__input-inner-wrapper"> <label>Axiom ' + (countAllRules + 1) + '</label> <input class="interface__input-field axioms" type="text" id="axiom' + (countAllRules + 1) + '" maxlength="1""> </div> <div class="interface__input-inner-wrapper"> <label>Regel ' + (countAllRules + 1) + '</label> <input class="interface__input-field rules" type="text" id="rule' + (countAllRules + 1) + '"> </div> </div>')
+    countAllRules++
+    if(btnRemove.disabled == true)
+        btnRemove.disabled = false;
+})
 
-// Functions for Eventlisteners
-function addNewRuleField() {
-    ruleWrapper.insertAdjacentHTML('beforeend', '<div class="interface__input-inner-wrapper"><label>Regel</label><input class="interface__input-field rules" type="text"></div>');
-    ruleWrapper.lastElementChild.classList.add("mb-100")
-    console.log(ruleWrapper.lastChild);
-}
+btnRemove.addEventListener('click', () => {
+    const allRulesLength = rulesWrapper.children.length
 
+    if(allRulesLength > 1) {
+        document.querySelector('#count' + countAllRules).remove()
+        countAllRules--
+    }
 
+    if(countAllRules <= 1)
+        btnRemove.disabled = true;
+})
 
+btnUpload.addEventListener('click', () => {
+    // add upload stuff here
+})
 
+btnDownload.addEventListener('click', () => {
+    // add download stuff here
+})
 
+btnGenerate.addEventListener('click', () => {
+    const axioms: string[] = new Array()
+    const rules: string[] = new Array()
 
+    document.querySelectorAll('.axioms').forEach(element => {
+        axioms.push((<HTMLInputElement> element).value)
+    })
 
+    document.querySelectorAll('.rules').forEach(element => {
+        rules.push((<HTMLInputElement> element).value)
+    })
 
+    const sentence: string = (<HTMLInputElement> document.querySelector('#sentence')).value
+    const iterations: number = parseInt((<HTMLInputElement> document.querySelector('#countIterations')).value)
+    const degrees: number = parseInt((<HTMLInputElement> document.querySelector('#degrees')).value)
+    const steplength: number = parseInt((<HTMLInputElement> document.querySelector('#steplength')).value)/10
+    
+    for (let i = 0; i < axioms.length; i++) {
+        ruleset.push(new Rule(axioms[i], rules[i]))
+    }
 
+    let lsys: LSystem = new LSystem(sentence, ruleset);
 
+    for (let i: number = 0; i < iterations; i++) {
+        lsys.generate()
+        console.log(lsys.getSentence())
+    }
 
+    let turtle: Turtle = new Turtle(lsys.getSentence(), steplength, Utils.DegreesToRadians(degrees))
 
-
-
-
-
-
+    init(turtle)
+    animate()
+})
 
 
 
@@ -72,17 +115,17 @@ function addNewRuleField() {
 // let turtle: Turtle = new Turtle(lsys.getSentence(), 0.5, (2 * Math.PI) / 3)
 
 // Tree
-ruleset.push(new Rule('F', 'FF+[+F-F-F]-[-F+F+F]'))
-let lsys: LSystem = new LSystem('F', ruleset)
-for (let i: number = 0; i < generations; i++) {
-    lsys.generate()
-    console.log(lsys.getSentence())
-}
-let turtle: Turtle = new Turtle(
-    lsys.getSentence(),
-    0.2,
-    Utils.DegreesToRadians(25)
-)
+// ruleset.push(new Rule('F', 'FF+[+F-F-F]-[-F+F+F]'))
+// let lsys: LSystem = new LSystem('F', ruleset)
+// for (let i: number = 0; i < generations; i++) {
+//     lsys.generate()
+//     console.log(lsys.getSentence())
+// }
+// let turtle: Turtle = new Turtle(
+//     lsys.getSentence(),
+//     0.2,
+//     Utils.DegreesToRadians(25)
+// )
 
 // Some Plant
 // ruleset.push(new Rule('F', 'F[+F]F[-F][F]'))
@@ -106,10 +149,7 @@ let turtle: Turtle = new Turtle(
 // }
 // let turtle: Turtle = new Turtle(lsys.getSentence(), 0.5, Utils.degreesToRadians(90))
 
-init()
-animate()
-
-function init() {
+function init(turtle: Turtle) {
     renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
