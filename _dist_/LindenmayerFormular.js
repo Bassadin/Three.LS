a6095abc9918bc58771b00de8fab1e3a7d905b25
manuel-pross <manuel-pross@users.noreply.github.com>
2021-01-06 10:48:31 +0000

    Automated GH Pages deploy 20cfb6d97d4ecebcae4c5452871c36a248ab300d

---
+++ _dist_/LindenmayerFormular.js
@@ -0,0 +1,65 @@
+import {Turtle3D} from "./Turtles/Turtle3D.js";
+import {Rule} from "./Rule.js";
+import {LSystem} from "./LSystem.js";
+import {Utils} from "./Utils.js";
+export class LindenmayerFormular {
+  constructor() {
+    this.btnAdd = document.querySelector("#btnAddRule");
+    this.btnRemove = document.querySelector("#btnRemoveRule");
+    this.btnUpload = document.querySelector("#btnUpload");
+    this.btnDownload = document.querySelector("#btnDownload");
+    this.rulesWrapper = document.querySelector("#rulesWrapper");
+    this.countAllRules = 1;
+    this.addListenerToAddButton();
+    this.addListenerToRemoveButton();
+  }
+  static getInstance() {
+    if (LindenmayerFormular.instance == void 0)
+      LindenmayerFormular.instance = new LindenmayerFormular();
+    return LindenmayerFormular.instance;
+  }
+  addListenerToAddButton() {
+    this.btnAdd.addEventListener("click", () => {
+      this.rulesWrapper.insertAdjacentHTML("beforeend", ' <div class="interface__rule-wrapper" style="margin-top: 1rem;" id="count' + (this.countAllRules + 1) + '"> <div class="interface__input-inner-wrapper"> <label>Axiom ' + (this.countAllRules + 1) + '</label> <input class="interface__input-field axioms" type="text" id="axiom' + (this.countAllRules + 1) + '" maxlength="1""> </div> <div class="interface__input-inner-wrapper"> <label>Regel ' + (this.countAllRules + 1) + '</label> <input class="interface__input-field rules" type="text" id="rule' + (this.countAllRules + 1) + '"> </div> </div>');
+      this.countAllRules++;
+      if (this.btnRemove.disabled == true)
+        this.btnRemove.disabled = false;
+    });
+  }
+  addListenerToRemoveButton() {
+    this.btnRemove.addEventListener("click", () => {
+      const allRulesLength = this.rulesWrapper.children.length;
+      if (allRulesLength > 1) {
+        document.querySelector("#count" + this.countAllRules).remove();
+        this.countAllRules--;
+      }
+      if (this.countAllRules <= 1)
+        this.btnRemove.disabled = true;
+    });
+  }
+  generateLSystemImage() {
+    const axioms = new Array();
+    const rules = new Array();
+    document.querySelectorAll(".axioms").forEach((element) => {
+      axioms.push(element.value.toUpperCase());
+    });
+    document.querySelectorAll(".rules").forEach((element) => {
+      rules.push(element.value.toUpperCase());
+    });
+    const sentence = document.querySelector("#sentence").value.toUpperCase();
+    const iterations = parseInt(document.querySelector("#countIterations").value);
+    const degrees = parseInt(document.querySelector("#degrees").value);
+    const steplength = parseInt(document.querySelector("#steplength").value) / 10;
+    let ruleset = [];
+    for (let i = 0; i < axioms.length; i++) {
+      ruleset.push(new Rule(axioms[i], rules[i]));
+    }
+    let lsys = new LSystem(sentence, ruleset);
+    console.time("L System generation");
+    for (let i = 0; i < iterations; i++)
+      lsys.generate();
+    console.timeEnd("L System generation");
+    let turtle = new Turtle3D(lsys.getSentence(), steplength, Utils.DegreesToRadians(degrees));
+    return turtle;
+  }
+}
--- _dist_/index.js
+++ _dist_/index.js
@@ -1,9 +1,6 @@
 import * as THREE from "../web_modules/three.js";
 import {TrackballControls} from "../web_modules/three/examples/jsm/controls/TrackballControls.js";
-import {Turtle3D} from "./Turtles/Turtle3D.js";
-import {Rule} from "./Rule.js";
-import {LSystem} from "./LSystem.js";
-import {Utils} from "./Utils.js";
+import {LindenmayerFormular} from "./LindenmayerFormular.js";
 import Stats from "../web_modules/stats-js.js";
 let stats = new Stats();
 stats.showPanel(0);
@@ -16,66 +13,24 @@ let renderer;
 let line;
 let controls;
 let clock = new THREE.Clock();
-let generations = 3;
-let countAllRules = 1;
-const btnAdd = document.querySelector("#btnAddRule");
-const btnRemove = document.querySelector("#btnRemoveRule");
-const btnUpload = document.querySelector("#btnUpload");
-const btnDownload = document.querySelector("#btnDownload");
 const btnGenerate = document.querySelector("#btnGenerate");
-btnRemove.disabled = true;
-const rulesWrapper = document.querySelector("#rulesWrapper");
-btnAdd.addEventListener("click", () => {
-  rulesWrapper.insertAdjacentHTML("beforeend", ' <div class="interface__rule-wrapper" style="margin-top: 1rem;" id="count' + (countAllRules + 1) + '"> <div class="interface__input-inner-wrapper"> <label>Axiom ' + (countAllRules + 1) + '</label> <input class="interface__input-field axioms" type="text" id="axiom' + (countAllRules + 1) + '" maxlength="1""> </div> <div class="interface__input-inner-wrapper"> <label>Regel ' + (countAllRules + 1) + '</label> <input class="interface__input-field rules" type="text" id="rule' + (countAllRules + 1) + '"> </div> </div>');
-  countAllRules++;
-  if (btnRemove.disabled == true)
-    btnRemove.disabled = false;
-});
-btnRemove.addEventListener("click", () => {
-  const allRulesLength = rulesWrapper.children.length;
-  if (allRulesLength > 1) {
-    document.querySelector("#count" + countAllRules).remove();
-    countAllRules--;
-  }
-  if (countAllRules <= 1)
-    btnRemove.disabled = true;
-});
-btnUpload.addEventListener("click", () => {
-});
-btnDownload.addEventListener("click", () => {
-});
-btnGenerate.addEventListener("click", generateLSystemImage);
-function generateLSystemImage() {
-  const axioms = new Array();
-  const rules = new Array();
-  document.querySelectorAll(".axioms").forEach((element) => {
-    axioms.push(element.value.toUpperCase());
-  });
-  document.querySelectorAll(".rules").forEach((element) => {
-    rules.push(element.value.toUpperCase());
-  });
-  const sentence = document.querySelector("#sentence").value.toUpperCase();
-  const iterations = parseInt(document.querySelector("#countIterations").value);
-  const degrees = parseInt(document.querySelector("#degrees").value);
-  const steplength = parseInt(document.querySelector("#steplength").value) / 10;
-  let ruleset = [];
-  for (let i = 0; i < axioms.length; i++) {
-    ruleset.push(new Rule(axioms[i], rules[i]));
-  }
-  let lsys = new LSystem(sentence, ruleset);
-  console.time("L System generation");
-  for (let i = 0; i < iterations; i++) {
-    lsys.generate();
-  }
-  console.timeEnd("L System generation");
-  let turtle = new Turtle3D(lsys.getSentence(), steplength, Utils.DegreesToRadians(degrees));
+const lindenmayerFormular = LindenmayerFormular.getInstance();
+let newTurtle = lindenmayerFormular.generateLSystemImage();
+if (scene !== void 0) {
+  repaint(newTurtle);
+} else {
+  init(newTurtle);
+  animate();
+}
+btnGenerate.addEventListener("click", () => {
+  let newTurtle2 = lindenmayerFormular.generateLSystemImage();
   if (scene !== void 0) {
-    repaint(turtle);
+    repaint(newTurtle2);
   } else {
-    init(turtle);
+    init(newTurtle2);
     animate();
   }
-}
+});
 function init(turtle) {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -107,4 +62,3 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-generateLSystemImage();
