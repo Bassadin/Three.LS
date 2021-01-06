20cfb6d97d4ecebcae4c5452871c36a248ab300d
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2021-01-06 11:47:03 +0100

    TASK: add a FormularClass as Singleton, remove stuff from index

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { Turtle3D } from './Turtles/Turtle3D'
 import { Rule } from './Rule'
 import { LSystem } from './LSystem'
 import { Utils } from './Utils'
+import { LindenmayerFormular } from './LindenmayerFormular'
 import Stats from 'stats-js'
 
 // #region Performance Stats
@@ -24,173 +25,30 @@ let line: THREE.Line
 let controls: TrackballControls
 let clock: Clock = new THREE.Clock()
 
-//L System stuff
-let generations: number = 3
-
-// Count all Rules
-let countAllRules = 1
-
-// Get Btns
-const btnAdd: HTMLInputElement = document.querySelector('#btnAddRule')
-const btnRemove: HTMLInputElement = document.querySelector('#btnRemoveRule')
-const btnUpload: HTMLInputElement = document.querySelector('#btnUpload')
-const btnDownload: HTMLInputElement = document.querySelector('#btnDownload')
 const btnGenerate: HTMLInputElement = document.querySelector('#btnGenerate')
-btnRemove.disabled = true
 
-//Necessary Elements
-const rulesWrapper: HTMLDivElement = document.querySelector('#rulesWrapper')
+const lindenmayerFormular: LindenmayerFormular = LindenmayerFormular.getInstance()
 
-// Eventlisteners for Btns
-btnAdd.addEventListener('click', () => {
-    rulesWrapper.insertAdjacentHTML(
-        'beforeend',
-        ' <div class="interface__rule-wrapper" style="margin-top: 1rem;" id="count' +
-            (countAllRules + 1) +
-            '"> <div class="interface__input-inner-wrapper"> <label>Axiom ' +
-            (countAllRules + 1) +
-            '</label> <input class="interface__input-field axioms" type="text" id="axiom' +
-            (countAllRules + 1) +
-            '" maxlength="1""> </div> <div class="interface__input-inner-wrapper"> <label>Regel ' +
-            (countAllRules + 1) +
-            '</label> <input class="interface__input-field rules" type="text" id="rule' +
-            (countAllRules + 1) +
-            '"> </div> </div>'
-    )
-    countAllRules++
-    if (btnRemove.disabled == true) btnRemove.disabled = false
-})
+let newTurtle: Turtle3D = lindenmayerFormular.generateLSystemImage()
 
-btnRemove.addEventListener('click', () => {
-    const allRulesLength = rulesWrapper.children.length
-
-    if (allRulesLength > 1) {
-        document.querySelector('#count' + countAllRules).remove()
-        countAllRules--
-    }
-
-    if (countAllRules <= 1) btnRemove.disabled = true
-})
-
-btnUpload.addEventListener('click', () => {
-    // add upload stuff here
-})
-
-btnDownload.addEventListener('click', () => {
-    // add download stuff here
-})
-
-btnGenerate.addEventListener('click', generateLSystemImage)
-
-function generateLSystemImage(): void {
-    const axioms: string[] = new Array()
-    const rules: string[] = new Array()
-
-    document.querySelectorAll('.axioms').forEach((element) => {
-        axioms.push((<HTMLInputElement>element).value.toUpperCase())
-    })
-
-    document.querySelectorAll('.rules').forEach((element) => {
-        rules.push((<HTMLInputElement>element).value.toUpperCase())
-    })
-
-    const sentence: string = (<HTMLInputElement>(
-        document.querySelector('#sentence')
-    )).value.toUpperCase()
-
-    const iterations: number = parseInt(
-        (<HTMLInputElement>document.querySelector('#countIterations')).value
-    )
-    const degrees: number = parseInt(
-        (<HTMLInputElement>document.querySelector('#degrees')).value
-    )
-    const steplength: number =
-        parseInt(
-            (<HTMLInputElement>document.querySelector('#steplength')).value
-        ) / 10
-
-    let ruleset: Rule[] = []
-
-    for (let i = 0; i < axioms.length; i++) {
-        ruleset.push(new Rule(axioms[i], rules[i]))
-    }
-
-    let lsys: LSystem = new LSystem(sentence, ruleset)
-
-    console.time('L System generation')
-    for (let i: number = 0; i < iterations; i++) {
-        lsys.generate()
-        // console.log(lsys.getSentence())
-    }
-    console.timeEnd('L System generation')
-
-    let turtle: Turtle3D = new Turtle3D(
-        lsys.getSentence(),
-        steplength,
-        Utils.DegreesToRadians(degrees)
-    )
-
-    if (scene !== undefined) {
-        repaint(turtle)
-    } else {
-        init(turtle)
-        animate()
-    }
+if (scene !== undefined) {
+    repaint(newTurtle)
+} else {
+    init(newTurtle)
+    animate()
 }
 
-// Square
-// ruleset.push(new Rule('F', 'F[F]-F+F[--F]+F-F'))
-// let lsys: LSystem = new LSystem('F-F-F-F', ruleset)
-// for (let i: number = 0; i < generations; i++) {
-//     lsys.generate()
-//     console.log(lsys.getSentence())
-// }
-// let turtle: Turtle = new Turtle(lsys.getSentence(), 0.5, Math.PI / 2)
 
-// Triangle
-// ruleset.push(new Rule('F', 'F--F--F--G'))
-// ruleset.push(new Rule('G', 'GG'))
-// let lsys: LSystem = new LSystem('F--F--F', ruleset)
-// for (let i: number = 0; i < generations; i++) {
-//     lsys.generate()
-//     console.log(lsys.getSentence())
-// }
-// let turtle: Turtle = new Turtle(lsys.getSentence(), 0.5, (2 * Math.PI) / 3)
+btnGenerate.addEventListener('click', () => {
+    let newTurtle: Turtle3D = lindenmayerFormular.generateLSystemImage();
 
-// Tree
-// ruleset.push(new Rule('F', 'FF+[+F-F-F]-[-F+F+F]'))
-// let lsys: LSystem = new LSystem('F', ruleset)
-// for (let i: number = 0; i < generations; i++) {
-//     lsys.generate()
-//     console.log(lsys.getSentence())
-// }
-// let turtle: Turtle = new Turtle(
-//     lsys.getSentence(),
-//     0.2,
-//     Utils.DegreesToRadians(25)
-// )
-
-// Some Plant
-// ruleset.push(new Rule('F', 'F[+F]F[-F][F]'))
-// let lsys: LSystem = new LSystem('F', ruleset)
-// for (let i: number = 0; i < generations; i++) {
-//     lsys.generate()
-//     console.log(lsys.getSentence())
-// }
-// let turtle: Turtle = new Turtle(
-//     lsys.getSentence(),
-//     0.2,
-//     Utils.DegreesToRadians(20)
-// )
-
-//Debugging
-// ruleset.push(new Rule('F', 'F+[F]--[F]+'))
-// let lsys: LSystem = new LSystem('F', ruleset)
-// for (let i: number = 0; i < generations; i++) {
-//     lsys.generate()
-//     console.log(lsys.getSentence())
-// }
-// let turtle: Turtle = new Turtle(lsys.getSentence(), 0.5, Utils.degreesToRadians(90))
+    if (scene !== undefined) {
+        repaint(newTurtle)
+    } else {
+        init(newTurtle)
+        animate()
+    }
+})
 
 function init(turtle: Turtle3D) {
     renderer = new THREE.WebGLRenderer()
@@ -240,5 +98,3 @@ function onWindowResize() {
 
     renderer.setSize(window.innerWidth, window.innerHeight)
 }
-
-generateLSystemImage()
