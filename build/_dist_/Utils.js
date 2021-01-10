d589df63f2eed0f4722d3ba93e1e1487edc0cbfe
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-10 16:48:16 +0100

    delete the build folder

--- build/_dist_/Utils.js
+++
@@ -1,9 +0,0 @@
-export class Utils {
-  static DegreesToRadians(degrees) {
-    let pi = Math.PI;
-    return degrees * (pi / 180);
-  }
-  static RandomNumber(min, max) {
-    return Math.random() * (+max - +min) + +min;
-  }
-}
