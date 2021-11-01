104947d36d86906c803387d6a5a28c87330e092e
Bastian Hodapp <bastianhodapp@gmail.com>
2021-11-01 20:11:43 +0100

    include new webxr/ar testing scene

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
 import * as THREE from 'three';
-import { Renderer, Scene } from 'three';
+import { Scene } from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 import { Turtle3D } from './Turtles/Turtle3D';
 import { LindenmayerFormular } from './LindenmayerFormular';
 import PerformanceStats from './PerformanceStats';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 
 export let scene: Scene;
 let camera: THREE.PerspectiveCamera;
-let renderer: Renderer;
+let renderer: THREE.WebGLRenderer;
 let controls: TrackballControls;
 
-const lindenmayerSettingsForm: LindenmayerFormular = LindenmayerFormular.getInstance();
+// Can we handle routes differently somehow? ~bas
+const windowLocationHref: string = window.location.href;
+const windowFileLocationName: string = windowLocationHref.substring(windowLocationHref.lastIndexOf('/'));
 
-const newTurtle: Turtle3D = lindenmayerSettingsForm.generateLSystemImage();
+switch (windowFileLocationName) {
+    case '/':
+        const lindenmayerSettingsForm: LindenmayerFormular = LindenmayerFormular.getInstance();
+        const newTurtle: Turtle3D = lindenmayerSettingsForm.generateLSystemImage();
 
-if (scene !== undefined) {
-    repaint(newTurtle);
-} else {
-    initTestingScene(newTurtle);
-    animate();
+        if (scene !== undefined) {
+            repaint(newTurtle);
+        } else {
+            initTestingScene(newTurtle, lindenmayerSettingsForm);
+            animate();
+        }
+        break;
+    case '/ar.html':
+        initArTestingScene();
+        animate();
+
+        break;
+    default:
+        console.error('Route not found');
 }
 
-function hookUpGenerateButtonEventListener() {
+function hookUpGenerateButtonEventListener(lindenmayerSettingsForm: LindenmayerFormular) {
     const btnGenerate: HTMLInputElement = document.querySelector('#btnGenerate');
     btnGenerate.addEventListener('click', (e) => {
         e.preventDefault();
@@ -30,15 +45,16 @@ function hookUpGenerateButtonEventListener() {
         if (scene !== undefined) {
             repaint(newTurtle);
         } else {
-            initTestingScene(newTurtle);
+            initTestingScene(newTurtle, lindenmayerSettingsForm);
             animate();
         }
     });
 }
 
-function initTestingScene(turtle: Turtle3D) {
+function initTestingScene(turtle: Turtle3D, lindenmayerSettingsForm: LindenmayerFormular) {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(renderer.domElement);
 
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
@@ -56,7 +72,44 @@ function initTestingScene(turtle: Turtle3D) {
 
     window.addEventListener('resize', onWindowResize, false);
 
-    hookUpGenerateButtonEventListener();
+    hookUpGenerateButtonEventListener(lindenmayerSettingsForm);
+}
+
+function initArTestingScene() {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+
+    scene = new THREE.Scene();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.xr.enabled = true;
+    document.body.appendChild(renderer.domElement);
+
+    document.body.appendChild(ARButton.createButton(renderer));
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    light.position.set(0.5, 1, 0.25);
+    scene.add(light);
+
+    // TODO: replace this geometrx with a generated tree somehow
+    const geometry = new THREE.CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+
+    function onSelect() {
+        const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+        mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+        scene.add(mesh);
+    }
+
+    const controller = renderer.xr.getController(0);
+    controller.addEventListener('select', onSelect);
+    scene.add(controller);
+
+    renderer.render(scene, camera);
+
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function repaint(turtle: Turtle3D) {
@@ -68,12 +121,15 @@ function repaint(turtle: Turtle3D) {
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
+    renderer.setAnimationLoop(render);
+}
 
-    //Performance Stats
-    PerformanceStats.instance.update();
+function render() {
+    renderer.render(scene, camera);
+    controls?.update(); // Only update controls if present
+
+    //Performance Stats update
+    PerformanceStats.instance?.update(); // Only update stats if present
 }
 
 function onWindowResize() {
