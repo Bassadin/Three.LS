33e2b4c99ebb3e63c586f2bb0beb9659d0d319eb
NickHaecker <NickHaecker@users.noreply.github.com>
2021-11-04 14:00:53 +0000

    Automated GH Pages deploy 9c15fbfb8f6b6fdff58668f576fb40c392ebe30c

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,34 +2,48 @@ import * as THREE from "../web_modules/three.js";
 import {TrackballControls} from "../web_modules/three/examples/jsm/controls/TrackballControls.js";
 import {LindenmayerFormular} from "./LindenmayerFormular.js";
 import PerformanceStats from "./PerformanceStats.js";
+import {ARButton} from "../web_modules/three/examples/jsm/webxr/ARButton.js";
 export let scene;
 let camera;
 let renderer;
 let controls;
-const lindenmayerSettingsForm = LindenmayerFormular.getInstance();
-const newTurtle = lindenmayerSettingsForm.generateLSystemImage();
-if (scene !== void 0) {
-  repaint(newTurtle);
-} else {
-  initTestingScene(newTurtle);
-  animate();
+const windowLocationHref = window.location.href;
+const windowFileLocationName = windowLocationHref.substring(windowLocationHref.lastIndexOf("/"));
+switch (windowFileLocationName) {
+  case "/":
+    const lindenmayerSettingsForm = LindenmayerFormular.getInstance();
+    const newTurtle = lindenmayerSettingsForm.generateLSystemImage();
+    if (scene !== void 0) {
+      repaint(newTurtle);
+    } else {
+      initTestingScene(newTurtle, lindenmayerSettingsForm);
+      animate();
+    }
+    break;
+  case "/ar.html":
+    initArTestingScene();
+    animate();
+    break;
+  default:
+    console.error("Route not found");
 }
-function hookUpGenerateButtonEventListener() {
+function hookUpGenerateButtonEventListener(lindenmayerSettingsForm) {
   const btnGenerate = document.querySelector("#btnGenerate");
   btnGenerate.addEventListener("click", (e) => {
     e.preventDefault();
-    const newTurtle2 = lindenmayerSettingsForm.generateLSystemImage();
+    const newTurtle = lindenmayerSettingsForm.generateLSystemImage();
     if (scene !== void 0) {
-      repaint(newTurtle2);
+      repaint(newTurtle);
     } else {
-      initTestingScene(newTurtle2);
+      initTestingScene(newTurtle, lindenmayerSettingsForm);
       animate();
     }
   });
 }
-function initTestingScene(turtle) {
+function initTestingScene(turtle, lindenmayerSettingsForm) {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1e4);
   controls = new TrackballControls(camera, renderer.domElement);
@@ -40,7 +54,33 @@ function initTestingScene(turtle) {
   turtle.addGeometryToScene(scene);
   renderer.render(scene, camera);
   window.addEventListener("resize", onWindowResize, false);
-  hookUpGenerateButtonEventListener();
+  hookUpGenerateButtonEventListener(lindenmayerSettingsForm);
+}
+function initArTestingScene() {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.xr.enabled = true;
+  document.body.appendChild(renderer.domElement);
+  document.body.appendChild(ARButton.createButton(renderer));
+  const light = new THREE.HemisphereLight(16777215, 12303359, 1);
+  light.position.set(0.5, 1, 0.25);
+  scene.add(light);
+  const geometry = new THREE.CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+  function onSelect() {
+    const material = new THREE.MeshPhongMaterial({color: 16777215 * Math.random()});
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+    mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+    scene.add(mesh);
+  }
+  const controller = renderer.xr.getController(0);
+  controller.addEventListener("select", onSelect);
+  scene.add(controller);
+  renderer.render(scene, camera);
+  window.addEventListener("resize", onWindowResize, false);
 }
 function repaint(turtle) {
   for (let i = scene.children.length - 1; i >= 0; i--) {
@@ -50,10 +90,12 @@ function repaint(turtle) {
   turtle.addGeometryToScene(scene);
 }
 function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
+  renderer.setAnimationLoop(render);
+}
+function render() {
   renderer.render(scene, camera);
-  PerformanceStats.instance.update();
+  controls?.update();
+  PerformanceStats.instance?.update();
 }
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
