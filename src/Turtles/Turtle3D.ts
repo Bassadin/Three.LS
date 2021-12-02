ee74e76dca4cc818bd9e72cfd39ca73899736d91
Bastian Hodapp <bastianhodapp@gmail.com>
2021-12-02 16:34:23 +0100

    move plane function to utils class

--- src/Turtles/Turtle3D.ts
+++ src/Turtles/Turtle3D.ts
@@ -13,6 +13,7 @@ import {
 import { BaseTurtle } from './BaseTurtle';
 import * as FragmentData from '../shaders/testShader/fragment';
 import * as VertexData from '../shaders/testShader/vertex';
+import { Utils } from '../Utils';
 
 export class Turtle3D extends BaseTurtle {
     public branchingIds: Set<number> = new Set();
@@ -146,7 +147,7 @@ export class Turtle3D extends BaseTurtle {
         // mesh.applyMatrix4(new Matrix4().makeTranslation(globalCenterPoint.x, globalCenterPoint.y, globalCenterPoint.z));
 
         console.timeEnd('Geometry creation');
-        scene.add(createPlane());
+        scene.add(Utils.createPlane());
     }
 
     move(): void {
@@ -156,26 +157,4 @@ export class Turtle3D extends BaseTurtle {
 
         this.currentPosition.add(absoluteMovement);
     }
-}
-function createPlane(): THREE.Mesh {
-    const bufferGeometry: THREE.BufferGeometry = new BufferGeometry();
-    const newColors = [Math.random() * 0.7 + 0.3, Math.random() * 0.7 + 0.3, Math.random() * 0.7 + 0.3];
-    const vertices: any[] = [];
-    const tris: number[] = [];
-    vertices[0] = [-5, -5, 2.5];
-    vertices[1] = [5, -5, 2.5];
-    vertices[2] = [5, -5, -2.5];
-    vertices[3] = [-5, -5, -2.5];
-    tris.push(...[...vertices[0], ...vertices[1], ...vertices[2], ...vertices[0], ...vertices[2], ...vertices[3]]);
-    const colorsArray: number[] = [...newColors, ...newColors, ...newColors, ...newColors, ...newColors, ...newColors];
-    bufferGeometry.setAttribute('position', new Float32BufferAttribute(tris, 3));
-
-    bufferGeometry.setAttribute('color', new Float32BufferAttribute(colorsArray, 3));
-
-    const material = new MeshBasicMaterial({
-        vertexColors: true,
-    });
-
-    const mesh = new Mesh(bufferGeometry, material);
-    return mesh;
 }
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,5 @@
+import { BufferGeometry, Float32BufferAttribute, Mesh, MeshBasicMaterial } from 'three';
+
 export class Utils {
     static DegreesToRadians(degrees: number): number {
         const pi: number = Math.PI;
@@ -6,5 +8,35 @@ export class Utils {
 
     static RandomNumber(min: number, max: number): number {
         return Math.random() * (+max - +min) + +min;
+    }
+
+    static createPlane(): THREE.Mesh {
+        const bufferGeometry: THREE.BufferGeometry = new BufferGeometry();
+        const newColors = [Math.random() * 0.7 + 0.3, Math.random() * 0.7 + 0.3, Math.random() * 0.7 + 0.3];
+        const vertices: any[] = [];
+        const tris: number[] = [];
+        vertices[0] = [-5, -5, 2.5];
+        vertices[1] = [5, -5, 2.5];
+        vertices[2] = [5, -5, -2.5];
+        vertices[3] = [-5, -5, -2.5];
+        tris.push(...[...vertices[0], ...vertices[1], ...vertices[2], ...vertices[0], ...vertices[2], ...vertices[3]]);
+        const colorsArray: number[] = [
+            ...newColors,
+            ...newColors,
+            ...newColors,
+            ...newColors,
+            ...newColors,
+            ...newColors,
+        ];
+        bufferGeometry.setAttribute('position', new Float32BufferAttribute(tris, 3));
+
+        bufferGeometry.setAttribute('color', new Float32BufferAttribute(colorsArray, 3));
+
+        const material = new MeshBasicMaterial({
+            vertexColors: true,
+        });
+
+        const mesh = new Mesh(bufferGeometry, material);
+        return mesh;
     }
 }
