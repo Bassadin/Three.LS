2684dd567ab1b215dc519fe56249064dd889e5b3
Bastian Hodapp <bastianhodapp@gmail.com>
2021-12-07 18:39:25 +0100

    Use AR reticle to spawn trees on the ground

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { Clock, Scene } from 'three';
+import { Clock, Mesh, Scene, XRHitTestSource } from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 import Turtle from './Turtle';
 import { LindenmayerFormular } from './LindenmayerFormular';
@@ -18,6 +18,12 @@ const sceneClock: Clock = new Clock();
 // Can we handle routes differently somehow? ~bas
 const windowLocationHref: string = window.location.href;
 const windowFileLocationName: string = windowLocationHref.substring(windowLocationHref.lastIndexOf('/'));
+
+//AR Stuff
+let reticle: Mesh;
+let hitTestSource: XRHitTestSource = null;
+let hitTestSourceRequested = false;
+let isARScene = false;
 
 switch (windowFileLocationName) {
     case '/':
@@ -60,6 +66,8 @@ export function generateAndRepaintLindenmayerMesh() {
 }
 
 function initTestingScene(turtle: Turtle) {
+    isARScene = false;
+
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -97,17 +105,19 @@ function initTestingScene(turtle: Turtle) {
 }
 
 function initArTestingScene() {
+    isARScene = true;
+
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.xr.enabled = true;
     document.body.appendChild(renderer.domElement);
 
-    document.body.appendChild(ARButton.createButton(renderer));
+    document.body.appendChild(ARButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     light.position.set(0.5, 1, 0.25);
@@ -122,7 +132,8 @@ function initArTestingScene() {
     function onSelect() {
         const turtle: Turtle = new Turtle(lsys.getSentence(), 1, Utils.DegreesToRadians(30));
         const turtleMesh = turtle.generateMeshObject();
-        turtleMesh.position.set(0, 0, -0.8).applyMatrix4(controller.matrixWorld);
+        // turtleMesh.position.set(0, 0, -0.8).applyMatrix4(controller.matrixWorld);
+        turtleMesh.position.setFromMatrixPosition(reticle.matrix);
         turtleMesh.scale.set(1, 1, 1);
         turtleMesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
         scene.add(turtleMesh);
@@ -131,6 +142,15 @@ function initArTestingScene() {
     const controller = renderer.xr.getController(0);
     controller.addEventListener('select', onSelect);
     scene.add(controller);
+
+    // Add AR target reticle
+    reticle = new THREE.Mesh(
+        new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+        new THREE.MeshBasicMaterial(),
+    );
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
 
     renderer.render(scene, camera);
 
@@ -149,7 +169,40 @@ function animate() {
     renderer.setAnimationLoop(render);
 }
 
-function render() {
+function render(timestamp, frame) {
+    if (frame && isARScene) {
+        const referenceSpace = renderer.xr.getReferenceSpace();
+        const session = renderer.xr.getSession();
+
+        if (hitTestSourceRequested === false) {
+            session.requestReferenceSpace('viewer').then(function (referenceSpace) {
+                session.requestHitTestSource({ space: referenceSpace }).then(function (source) {
+                    hitTestSource = source;
+                });
+            });
+
+            session.addEventListener('end', function () {
+                hitTestSourceRequested = false;
+                hitTestSource = null;
+            });
+
+            hitTestSourceRequested = true;
+        }
+
+        if (hitTestSource) {
+            const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+            if (hitTestResults.length) {
+                const hit = hitTestResults[0];
+
+                reticle.visible = true;
+                reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+            } else {
+                reticle.visible = false;
+            }
+        }
+    }
+
     renderer.render(scene, camera);
 
     controls?.update(); // Only update controls if present
